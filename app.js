b680c5d34d23fdeb4c40e24d532201623b1aaefa
sudharm2004 <sudharmjadhav2004@gmail.com>
2023-01-23 22:27:24 +0530

    day 3 completed

--- app.js
+++ app.js
@@ -14,9 +14,28 @@ import  ReactDOM  from 'react-dom/client';
 
 
 //USING REACT TO CREATE AND RENDER WEB PAGES
-const heading1 = React.createElement("h1", {id:'heading1'}, "hello world1");
-const heading2 = React.createElement("h1", {id:'heading1'}, "hello world2");
-const heading3 = React.createElement("h1", {id:'heading1'}, "hello world3");
-const container= React.createElement('div',{className:'container'},[heading1,heading2,heading3]);
+// const heading1 = React.createElement("h1", {id:'heading1',key:"h1"}, "hello world1");
+// const heading2 = React.createElement("h2", {id:'heading1',key:"h2"}, "hello world2");
+// const heading3 = React.createElement("h3", {id:'heading1',key:"h3"}, "hello world3");
+// const container= React.createElement('div',{className:'container'},[heading1,heading2,heading3]);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(container);
+
+
+//using jsx and components
+const heading=<h1>hello world!</h1>;
+const Header=()=>{
+    return (
+        <header>
+            <img src="" alt="logo"/>
+            <div className="searchBar">
+
+            <input type="text" name="search key" id='headerSearchBar' placeholder='Enter the product name to search'/>
+            <button><i class="fas fa-search"></i></button>
+            </div>
+            <img src="" alt='userIcon'/>
+        </header>
+    )
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
+root.render(<Header/>);
